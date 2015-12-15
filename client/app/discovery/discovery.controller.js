63f0dbfdeafa743dbed05a766c23ffbbdc29c32d
Samuel Bowler <samuel.bowler@gmail.com>
2015-12-15 15:54:21 -0600

    feat/album - images more responsive (need resizing)

--- client/app/discovery/discovery.controller.js
+++ client/app/discovery/discovery.controller.js
@@ -3,21 +3,95 @@
 angular.module('culturalystApp')
   .controller('DiscoveryCtrl', function ($scope) {
     $scope.message = 'Hello';
-    $scope.medium = ['Music','Writing','Film'];
+    $scope.medium = ['Comics', 'Crafts','Culture Bearers','Design','Fashion','Film & Video','Games','Music','Performing Arts','Photography','Visual Arts','Writing'];
     $scope.submedium = {
-    	'Music': ['Blues','Violin','Singer','Bluegrass']
+    	'Comics': ['Digital']
     };
 
     $scope.loadSubMediums = function(medium){
     	if (typeof $scope.submedium[medium] !== 'undefined'){
     		return;
     	}
-    	if (medium === 'Writing'){
+    	if (medium === 'Crafts'){
     		setTimeout(function(){
     			$scope.$apply(function(){
-    				$scope.submedium[medium] = ['Poetry','NonFiction','Fiction']; 
+    				$scope.submedium[medium] = ['Knitting','Crochet']; 
     			});
     		},200);
     	}
+    	if (medium === "Culture Bearers"){
+    		setTimeout(function(){
+    			$scope.$apply(function(){
+    				$scope.submedium[medium] = ['Mardi Gras Indians', 'Social Aid & Pleasure Clubs'];
+    			})
+    		},200);
+    	}
+    	if (medium === "Design"){
+    		setTimeout(function(){
+    			$scope.$apply(function(){
+    				$scope.submedium[medium] = ['TBD', 'TBD','TBD'];
+    			})
+    		},200);
+    	}
+    	if (medium === "Fashion"){
+    		setTimeout(function(){
+    			$scope.$apply(function(){
+    				$scope.submedium[medium] = ['TBD', 'TBD','TBD'];
+    			})
+    		},200);
+    	}
+    	if (medium === "Film & Video"){
+    		setTimeout(function(){
+    			$scope.$apply(function(){
+    				$scope.submedium[medium] = ['TBD', 'TBD','TBD'];
+    			})
+    		},200);
+    	}
+    	if (medium === "Games"){
+    		setTimeout(function(){
+    			$scope.$apply(function(){
+    				$scope.submedium[medium] = ['TBD', 'TBD','TBD'];
+    			})
+    		},200);
+    	}
+    	if (medium === "Music"){
+    		setTimeout(function(){
+    			$scope.$apply(function(){
+    				$scope.submedium[medium] = ['TBD', 'TBD','TBD'];
+    			})
+    		},200);
+    	}
+    	if (medium === "Performing Arts"){
+    		setTimeout(function(){
+    			$scope.$apply(function(){
+    				$scope.submedium[medium] = ['TBD', 'TBD','TBD'];
+    			})
+    		},200);
+    	}
+    	if (medium === "Photography"){
+    		setTimeout(function(){
+    			$scope.$apply(function(){
+    				$scope.submedium[medium] = ['TBD', 'TBD','TBD'];
+    			})
+    		},200);
+    	}
+    	if (medium === "Visual Arts"){
+    		setTimeout(function(){
+    			$scope.$apply(function(){
+    				$scope.submedium[medium] = ['TBD', 'TBD','TBD'];
+    			})
+    		},200);
+    	}
+    	if (medium === "Writing"){
+    		setTimeout(function(){
+    			$scope.$apply(function(){
+    				$scope.submedium[medium] = ['TBD', 'TBD','TBD'];
+    			})
+    		},200);
+    	}
     };
+
+    $scope.test = function(){
+    	console.log("this was clicked");
+    }
   });
--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -8,11 +8,6 @@ class MainController {
     this.$http = $http;
     this.awesomeThings = [];
 
-    $http.get('/api/things').then(response => {
-      this.awesomeThings = response.data;
-      socket.syncUpdates('thing', this.awesomeThings);
-    });
-
     $scope.$on('$destroy', function() {
       socket.unsyncUpdates('thing');
     });
@@ -44,16 +39,6 @@ class MainController {
       }];
   }
 
-  addThing() {
-    if (this.newThing) {
-      this.$http.post('/api/things', { name: this.newThing });
-      this.newThing = '';
-    }
-  }
-
-  deleteThing(thing) {
-    this.$http.delete('/api/things/' + thing._id);
-  }
 }
 
 angular.module('culturalystApp')
