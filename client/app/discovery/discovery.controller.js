7e549064f1ce428b870b3dfb53e9a7d45803fdca
Samuel Bowler <samuel.bowler@gmail.com>
2015-12-17 13:29:09 -0600

    feat/artistCard: Working artist cards feat/artist should take you to individual artist page by id
    working on artist profile
    
    Merge pull request #1 from ahrobinson/feat/artist
    
    Feat/artist should take you to individual artist page by id
    fixed linting errors
    
    Merge pull request #11 from ahrobinson/master
    
    fix linting errors
    About to rebase
    
    Forgot to start new branch
    
    About to create a factory
    
    Working medium bars with selected media available
    
    Drop downs send get request.
    
    Merge pull request #13 from bkentropy/feat/disc
    
    Feat/disc
    feat/album - getting ready for rebase
    
    feat/album - staging commits for rebase
    
    feat/album - squashing conflicts during rebase
    
    feat/album - rebasing
    
    feat/artistCard - working artistCards

--- client/app/discovery/discovery.controller.js
+++ client/app/discovery/discovery.controller.js
@@ -1,23 +1,66 @@
 'use strict';
 
 angular.module('culturalystApp')
-  .controller('DiscoveryCtrl', function ($scope) {
-    $scope.message = 'Hello';
-    $scope.medium = ['Music','Writing','Film'];
-    $scope.submedium = {
-    	'Music': ['Blues','Violin','Singer','Bluegrass']
-    };
+  .controller('DiscoveryCtrl', function ($scope, $http, MediaList) {
+
+    $scope.mediaList = MediaList.getMediaList();
+    $scope.artists = [];
 
     $scope.loadSubMediums = function(medium){
-    	if (typeof $scope.submedium[medium] !== 'undefined'){
-    		return;
-    	}
-    	if (medium === 'Writing'){
-    		setTimeout(function(){
-    			$scope.$apply(function(){
-    				$scope.submedium[medium] = ['Poetry','NonFiction','Fiction']; 
-    			});
-    		},200);
-    	}
+      console.log(medium.name)
+      $scope.selectedMedium = medium.name;
+      $scope.submedia = medium.submedia;
     };
+
+    $scope.getArtists = function() {
+      $http.get('/api/users/discovery/' + $scope.selectedMedium + '/' + $scope.selectedSubmedium).then(function(response) {
+        $scope.artists = response.data;
+      })
+    }
+    
+
+    $scope.featured = [{
+        name: 'Sam Bowler',
+        medium: 'Sculpture',
+        img:'cover_photo_1.png'
+      }, {
+        name: 'Mykia Smith', 
+        medium: 'Writing',
+        img:'cover_photo_2.png'
+      },{
+        name: 'Alice Green', 
+        medium: 'Musician',
+        img:'cover_photo_3.png'
+      },{
+        name: 'Alon Robinson', 
+        medium: 'Musician',
+        img:'cover_photo_5.png'
+      },{
+        name: 'Ryan Bascle', 
+        medium: 'Musician',
+        img:'cover_photo_5.png'
+      }, {
+        name: 'Brian Kustra', 
+        medium: 'Musician',
+        img:'cover_photo_3.png'
+      },{
+        name: 'John Fraboni', 
+        medium: 'Musician',
+        img:'cover_photo_1.png'
+      },{
+        name: 'Max Gaudin', 
+        medium: 'Musician',
+        img:'cover_photo_2.png'
+      },{
+        name: 'Raisin Canes', 
+        medium: 'Musician',
+        img:'cover_photo_3.png'
+      }];
+  })
+  .controller('SearcherCtrl', function ($scope) {
+    $scope.currentSel = function() {
+      console.log($scope.selectedMedium);
+
+    }
   });
+  
